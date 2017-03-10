16777f967ac85b181097ee2c5f58dbb1c7832186
Max Veytsman <maxim@ontoillogical.com>
2017-03-09 23:55:00 -0500

    slightly more chatlike

--- lib/sshchat/room.ex
+++ lib/sshchat/room.ex
@@ -10,12 +10,14 @@ defmodule SSHChat.Room do
   end
 
   def register(pid, name) do
-    # let's say for now that the user list is going to be a map from peer addresses to pids
+    # let's say for now that the user list is going to be a map from peer addresses to pods
     Agent.update(@name, &Map.put(&1, pid, name))
+    announce("#{name} joined")
   end
 
   def unregister(pid) do
-    Agent.update(@name, &Map.delete(&1, pid))
+    name = Agent.get_and_update(@name, &Map.pop(&1, pid))
+    announce("#{name} left")
   end
 
   def inspect() do
@@ -26,11 +28,16 @@ defmodule SSHChat.Room do
     Agent.get(@name, &Map.get(&1, pid))
   end
 
+  def announce(message) do
+    Agent.get(@name, &Map.keys(&1))
+    |> Enum.each(fn(pid) -> SSHChat.Session.send_message(pid, " * #{message}") end)
+  end
+
   def send_from(pid, message) do
     name = get_name(pid)
     #SSHChat.Session.send_message(pid, "<you> #{message}")
     Agent.get(@name, &Map.delete(&1, pid)) # send to everyone else
     |> Map.keys
-    |> Enum.each fn(pid) -> SSHChat.Session.send_message(pid, "<#{name}> #{message}") end
-    end
+    |> Enum.each(fn(pid) -> SSHChat.Session.send_message(pid, "#{name}: #{message}") end)
+  end
 end
