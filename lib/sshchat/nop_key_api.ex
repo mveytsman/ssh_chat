653a31ed66a3c4d194ceae245ca24f4e7411e986
Max Veytsman <maxim@ontoillogical.com>
2017-03-09 23:26:36 -0500

    typo

--- lib/sshchat/nop_key_api.ex
+++ lib/sshchat/nop_key_api.ex
@@ -7,7 +7,7 @@ defmodule SSHChat.NopKeyApi do
 
   def host_key(algorithm, props) do
     # delegate to the ssh file key api
-    :ssh_file.host_ykey(algorithm, props)
+    :ssh_file.host_key(algorithm, props)
   end
 
   def is_auth_key(_key, _user, _options) do
